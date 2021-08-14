68bc631229270ec44b7c6482f3ad5a66efcfb0d7
banmuyutian <595817320@qq.com>
2021-08-14 13:06:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/fw876/helloworld.git package/
+git clone https://github.com/fw876/helloworld.git package/helloworld
